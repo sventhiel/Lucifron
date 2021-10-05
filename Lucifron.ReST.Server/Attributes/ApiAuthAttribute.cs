3f7379e87aa7f2c71d1eba70a3c8fad70717f2f4
Sven Thiel <55701246+sventhiel@users.noreply.github.com>
2021-10-05 09:38:25 +0200

    implement logic to check access permissions for api calls

--- Lucifron.ReST.Server/Attributes/ApiAuthAttribute.cs
+++ Lucifron.ReST.Server/Attributes/ApiAuthAttribute.cs
@@ -1,4 +1,9 @@
-﻿using System.Web.Http;
+﻿using LiteDB;
+using Lucifron.ReST.Server.Services;
+using System.Configuration;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
 using System.Web.Http.Controllers;
 
 namespace Lucifron.ReST.Server.Attributes
@@ -9,18 +14,27 @@ namespace Lucifron.ReST.Server.Attributes
         {
             //
             // Preparation
-            //var token = actionContext.Request.Headers.Authorization?.ToString().Substring("Bearer ".Length).Trim();
-            //string ipv4 = HttpContext.Current.Request.UserHostAddress;
+            var token = actionContext.Request.Headers.Authorization?.ToString().Substring("Bearer ".Length).Trim();
+            string ipv4 = HttpContext.Current.Request.UserHostAddress;
 
-            //var clientService = new ClientService(new ConnectionString(""));
-            //var user = clientService.FindByIPv4AndToken(ipv4, token);
+            //
+            // Admin
+            if (ConfigurationManager.AppSettings["AdminToken"] == token)
+                return;
 
-            //if (user == null)
-            //{
-            //    actionContext.Response = new HttpResponseMessage(System.Net.HttpStatusCode.Forbidden);
-            //    actionContext.Response.Content = new StringContent("Token is not valid.");
-            //    return;
-            //}
+            //
+            // User
+            var userService = new UserService(new ConnectionString(ConfigurationManager.ConnectionStrings["Lucifron"].ConnectionString));
+            var user = userService.FindByIPv4AndToken(ipv4, token);
+
+            if (user != null)
+                return;
+          
+            //
+            // Error
+            actionContext.Response = new HttpResponseMessage(System.Net.HttpStatusCode.Forbidden);
+            actionContext.Response.Content = new StringContent("Token is not valid.");
+            return;
         }
     }
 }
