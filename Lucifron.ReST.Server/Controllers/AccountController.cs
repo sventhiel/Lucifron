702fbd9c74dddf86dc41cc4313f7b72640aa1b77
Sven Thiel <55701246+sventhiel@users.noreply.github.com>
2021-10-05 19:04:26 +0200

    code maid clean up

---
+++ Lucifron.ReST.Server/Controllers/AccountController.cs
@@ -0,0 +1,45 @@
+﻿using Lucifron.ReST.Server.Models;
+using System.Configuration;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace Lucifron.ReST.Server.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult SignIn()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult SignIn(SignInModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                if (ConfigurationManager.AppSettings["Admin_Token"] == model.Token)
+                {
+                    FormsAuthentication.SetAuthCookie(ConfigurationManager.AppSettings["Admin_Name"], false);
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "The token is invalid!");
+                }
+            }
+            return View(model);
+        }
+
+        public ActionResult SignOut()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
---
+++ Lucifron.ReST.Server/Controllers/UsersController.cs
@@ -0,0 +1,29 @@
+﻿using LiteDB;
+using Lucifron.ReST.Server.Services;
+using System.Configuration;
+using System.Web.Mvc;
+
+namespace Lucifron.ReST.Server.Controllers
+{
+    public class UsersController : Controller
+    {
+        // GET: Users
+        [Authorize]
+        public ActionResult Index()
+        {
+            var userService = new UserService(new ConnectionString(ConfigurationManager.ConnectionStrings["Lucifron"].ConnectionString));
+
+            var users = userService.Get();
+            //userService.Create("test", "::1", "jexis");
+
+            return View();
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+
+    }
+}
