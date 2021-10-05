486d79d8a77bafd3b88c996a27ec08f187b0086b
Sven Thiel <55701246+sventhiel@users.noreply.github.com>
2021-10-05 09:40:09 +0200

    add property to set sub prefix of dois for user

--- Lucifron.ReST.Server/Entities/User.cs
+++ Lucifron.ReST.Server/Entities/User.cs
@@ -11,5 +11,6 @@ namespace Lucifron.ReST.Server.Entities
         public string Name { get; set; }
         public string IPv4 { get; set; }
         public string Token { get; set; }
+        public string Prefix { get; set; }
     }
 }
