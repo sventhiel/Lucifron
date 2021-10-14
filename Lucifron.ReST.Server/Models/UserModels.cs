32bd0684fdebc9026a7f4980037384982d868307
Sven Thiel <55701246+sventhiel@users.noreply.github.com>
2021-10-14 11:03:09 +0200

    add models for reading and creating users

--- Lucifron.ReST.Server/Models/UserModels.cs
+++ Lucifron.ReST.Server/Models/UserModels.cs
@@ -1,6 +1,37 @@
-﻿namespace Lucifron.ReST.Server.Models
+﻿using Lucifron.ReST.Server.Entities;
+using System.ComponentModel.DataAnnotations;
+
+namespace Lucifron.ReST.Server.Models
 {
-    public class UserModels
+    public class ReadUserModel
     {
+        public string Name { get; set; }
+        public string Prefix { get; set; }
+        public string IPv4 { get; set; }
+        public string Token { get; set; }
+
+        public static ReadUserModel Convert(User user)
+        {
+            return new ReadUserModel()
+            {
+                Name = user.Name,
+                Prefix = user.Prefix,
+                IPv4 = user.IPv4,
+                Token = user.Token
+            };
+        }
+    }
+
+    public class CreateUserModel
+    {
+        [Required]
+        [Display(Name = "Name")]
+        public string Name { get; set; }
+        [Required]
+        [Display(Name = "Prefix")]
+        public string Prefix { get; set; }
+        [Required]
+        [Display(Name = "IPv4")]
+        public string IPv4 { get; set; }
     }
 }
