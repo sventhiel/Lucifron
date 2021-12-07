14524cc3492374a7e048a8f132079a991290c86a
Sven Thiel <55701246+sventhiel@users.noreply.github.com>
2021-12-07 10:26:05 +0100

    add new entity related to server connections

---
+++ Lucifron.ReST.Server/Entities/Credential.cs
@@ -0,0 +1,10 @@
+﻿namespace Lucifron.ReST.Server.Entities
+{
+    public class Credential
+    {
+        public long Id { get; set; }
+        public string User { get; set; }
+        public string Password { get; set; }
+        public string Host { get; set; }
+    }
+}
