08dd05f2f1a285cea3be15eb7270223462628ab7
Sven Thiel <55701246+sventhiel@users.noreply.github.com>
2022-03-03 01:15:22 +0100

    add models for error messages as json

---
+++ Lucifron.ReST.Server/Models/ErrorModels.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Lucifron.ReST.Server.Models
+{
+    public class ErrorModel
+    {
+        [JsonProperty("source")]
+        public string Source { get; set; }
+
+        [JsonProperty("uid")]
+        public string UId { get; set; }
+
+        [JsonProperty("title")]
+        public string Title { get; set; }
+    }
+
+    public class ErrorsModel
+    {
+        [JsonProperty("errors")]
+        public List<ErrorModel> Errors { get; set; }
+    }
+}
