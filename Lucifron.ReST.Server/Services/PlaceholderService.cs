ec76b3905fdb01d962e6b7ab374456a26c16f558
Sven Thiel <55701246+sventhiel@users.noreply.github.com>
2022-04-11 08:51:55 +0200

    implementation of service layer for placeholders

---
+++ Lucifron.ReST.Server/Services/PlaceholderService.cs
@@ -0,0 +1,71 @@
+﻿using LiteDB;
+using Lucifron.ReST.Server.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Lucifron.ReST.Server.Services
+{
+    public class PlaceholderService
+    {
+        private readonly ConnectionString _connectionString;
+
+        public PlaceholderService(ConnectionString connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public long Create(string expression, string regularExpression, long userId)
+        {
+            using (var db = new LiteDatabase(_connectionString))
+            {
+                var col = db.GetCollection<Placeholder>("placeholders");
+                var users = db.GetCollection<User>("users");
+
+                var placeholder = new Placeholder()
+                {
+                    Expression = expression,
+                    RegularExpression = regularExpression,
+                    User = users.FindById(userId)
+                };
+
+                return col.Insert(placeholder);
+            }
+        }
+
+        public bool Delete(long id)
+        {
+            using (var db = new LiteDatabase(_connectionString))
+            {
+                var col = db.GetCollection<Placeholder>("placeholders");
+
+                return col.Delete(id);
+            }
+        }
+
+        public List<Placeholder> Find()
+        {
+            List<Placeholder> placeholders = null;
+
+            using (var db = new LiteDatabase(_connectionString))
+            {
+                var col = db.GetCollection<Placeholder>("placeholders");
+
+                placeholders = col.Query().ToList();
+            }
+
+            return placeholders;
+        }
+
+        public Dictionary<string, string> FindByUserId(long userId)
+        {
+            using (var db = new LiteDatabase(_connectionString))
+            {
+                var col = db.GetCollection<Placeholder>("placeholders");
+
+                return col.Include(c => c.User).Find(c => c.User.Id == userId).Select(c => new KeyValuePair<string, string>(c.Name, c.RegularExpression)).ToDictionary(item => item.Key, item => item.Value);
+            }
+        }
+    }
+}
