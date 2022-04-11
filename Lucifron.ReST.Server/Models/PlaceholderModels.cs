310b5bcc770a3e30183c93c32159f0bd703ed464
Sven Thiel <55701246+sventhiel@users.noreply.github.com>
2022-04-11 08:51:35 +0200

    read and create models for placeholders

---
+++ Lucifron.ReST.Server/Models/PlaceholderModels.cs
@@ -0,0 +1,41 @@
+﻿using Lucifron.ReST.Server.Entities;
+using System.ComponentModel.DataAnnotations;
+
+namespace Lucifron.ReST.Server.Models
+{
+    public class ReadPlaceholderModel
+    {
+        public long Id { get; set; }
+        public string Expression { get; set; }
+        public string RegularExpression { get; set; }
+        public long UserId { get; set; }
+        public string UserName { get; set; }
+
+        public static ReadPlaceholderModel Convert(Placeholder placeholder)
+        {
+            return new ReadPlaceholderModel()
+            {
+                Id = placeholder.Id,
+                Expression = placeholder.Expression,
+                RegularExpression = placeholder.RegularExpression,
+                UserId = placeholder.User.Id,
+                UserName = placeholder.User.Name
+            };
+        }
+    }
+
+    public class CreatePlaceholderModel
+    {
+        [Required]
+        [Display(Name = "Expression")]
+        public string Expression { get; set; }
+
+        [Required]
+        [Display(Name = "RegularExpression")]
+        public string RegularExpression { get; set; }
+
+        [Required]
+        [Display(Name = "UserId")]
+        public long UserId { get; set; }
+    }
+}
