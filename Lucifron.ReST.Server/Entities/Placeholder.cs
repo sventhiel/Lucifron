187df9004a70da89ccaebac8dd67c2c9ddcc50c1
Sven Thiel <55701246+sventhiel@users.noreply.github.com>
2022-04-11 08:51:03 +0200

    entity for placeholder and replacements at DOI creation

---
+++ Lucifron.ReST.Server/Entities/Placeholder.cs
@@ -0,0 +1,18 @@
+﻿using LiteDB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Lucifron.ReST.Server.Entities
+{
+    public class Placeholder
+    {
+        public long Id { get; set; }
+        public string Expression { get; set; }
+        public string RegularExpression { get; set; }
+
+        [BsonRef("users")]
+        public User User { get; set; }
+    }
+}
