7cd3237f70a86f8e65ee8cc82c240da97dc7cc00
Sven Thiel <55701246+sventhiel@users.noreply.github.com>
2022-04-14 10:38:04 +0200

    update service within client

---
+++ Lucifron.ReST.Client/Services/DOIService.cs
@@ -0,0 +1,49 @@
+﻿using RestSharp;
+using RestSharp.Authenticators;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lucifron.ReST.Client.Services
+{
+    public interface IDOIService
+    {
+
+    }
+
+    public class DOIService
+    {
+        private string _Host;
+        private string _Token;
+
+        public DOIService(string host, string token)
+        {
+            _Host = host;
+            _Token = token;
+        }
+
+        public string Create(Dictionary<string, string> placeholders = null)
+        {
+            try
+            {
+                var client = new RestClient(_Host);
+                client.Authenticator = new JwtAuthenticator(_Token);
+
+                var request = new RestRequest($"api/dois", Method.POST).AddJsonBody(placeholders);
+                var response = client.Execute(request);
+
+                if (!response.IsSuccessful)
+                    return string.Empty;
+
+                return response.Content;
+            }
+            catch(Exception ex)
+            {
+                return string.Empty;
+            }
+        }
+    }
+}
