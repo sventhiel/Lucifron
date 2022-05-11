b0620792c8bae0dc5a3e5ca02ef9f8dd02f32ff1
Sven Thiel <55701246+sventhiel@users.noreply.github.com>
2022-05-11 11:39:45 +0200

    unify and extend data cite model

--- Lucifron.ReST.Library/Models/DataCiteModels.cs
+++ Lucifron.ReST.Library/Models/DataCiteModels.cs
@@ -6,29 +6,22 @@ using System.Runtime.Serialization;
 namespace Lucifron.ReST.Library.Models
 {
     [JsonConverter(typeof(JsonPathConverter))]
-    public class CreateDataCiteModel
+    public class DataCiteModel
     {
         #region data
 
         [JsonProperty("data.id")]
         public string Id { get; set; }
 
+        [JsonRequired]
         [JsonProperty("data.type")]
         public DataCiteType Type { get; set; }
 
         #region data.attributes
 
-        [JsonProperty("data.attributes.creators")]
-        public List<DataCiteCreator> Creators { get; set; }
-
-        [JsonProperty("data.attributes.contributors")]
-        public List<DataCiteCreator> Contributors { get; set; }
-
-        [JsonProperty("data.attributes.descriptions")]
-        public List<DataCiteDescription> Descriptions { get; set; }
-
+        [JsonRequired]
         [JsonProperty("data.attributes.doi")]
-        public string DOI { get; set; }
+        public string Doi { get; set; }
 
         [JsonProperty("data.attributes.prefix")]
         public string Prefix { get; set; }
@@ -36,48 +29,92 @@ namespace Lucifron.ReST.Library.Models
         [JsonProperty("data.attributes.suffix")]
         public string Suffix { get; set; }
 
+        [JsonRequired]
         [JsonProperty("data.attributes.event")]
         public DataCiteEventType Event { get; set; }
 
-        [JsonProperty("data.attributes.publicationYear")]
-        public int PublicationYear { get; set; }
+        [JsonProperty("data.attributes.identifiers")]
+        public List<DataCiteIdentifier> Identifiers { get; set; }
 
+        [JsonRequired]
+        [JsonProperty("data.attributes.creators")]
+        public List<DataCiteCreator> Creators { get; set; }
+
+        [JsonRequired]
+        [JsonProperty("data.attributes.titles")]
+        public List<DataCiteTitle> Titles { get; set; }
+
+        [JsonRequired]
         [JsonProperty("data.attributes.publisher")]
         public string Publisher { get; set; }
+
+        [JsonRequired]
+        [JsonProperty("data.attributes.publicationYear")]
+        public int PublicationYear { get; set; }
 
         [JsonProperty("data.attributes.subjects")]
         public List<DataCiteSubject> Subjects { get; set; }
 
-        [JsonProperty("data.attributes.titles")]
-        public List<DataCiteTitle> Titles { get; set; }
+        [JsonProperty("data.attributes.contributors")]
+        public List<DataCiteCreator> Contributors { get; set; }
+
+        [JsonProperty("data.attributes.dates")]
+        public List<DataCiteDate> Dates { get; set; }
+
+        [JsonProperty("data.attributes.language")]
+        public string Language { get; set; }
 
         #region data.attributes.types
 
+        [JsonRequired]
         [JsonProperty("data.attributes.types.resourceTypeGeneral")]
         public DataCiteResourceType ResourceTypeGeneral { get; set; }
 
+        [JsonRequired]
         [JsonProperty("data.attributes.types.resourceType")]
         public string ResourceType { get; set; }
 
+        [JsonProperty("data.attributes.types.schemaOrg")]
+        public string SchemaOrg { get; set; }
+
+        [JsonProperty("data.attributes.types.bibtex")]
+        public string Bibtex { get; set; }
+
+        [JsonProperty("data.attributes.types.citeproc")]
+        public string Citeproc { get; set; }
+
+        [JsonProperty("data.attributes.types.ris")]
+        public string Ris { get; set; }
+
         #endregion data.attributes.types
+
+        // Related Identifiers
+
+        [JsonProperty("data.attributes.version")]
+        public string Version { get; set; }
 
         [JsonProperty("data.attributes.url")]
         public string URL { get; set; }
+
+        [JsonProperty("data.attributes.descriptions")]
+        public List<DataCiteDescription> Descriptions { get; set; }
 
         #endregion data.attributes
 
         #endregion
 
-        public CreateDataCiteModel()
+        public DataCiteModel()
         {
             Creators = new List<DataCiteCreator>();
             Contributors = new List<DataCiteCreator>();
+            Dates = new List<DataCiteDate>();
             Descriptions = new List<DataCiteDescription>();
+            Identifiers = new List<DataCiteIdentifier>();
             Subjects = new List<DataCiteSubject>();
             Titles = new List<DataCiteTitle>();
         }
 
-        public static CreateDataCiteModel Deserialize(string json)
+        public static DataCiteModel Deserialize(string json)
         {
             var jsonSettings = new JsonSerializerSettings
             {
@@ -85,32 +122,7 @@ namespace Lucifron.ReST.Library.Models
                 DefaultValueHandling = DefaultValueHandling.IgnoreAndPopulate
             };
 
-            return JsonConvert.DeserializeObject<CreateDataCiteModel>(json, jsonSettings);
-        }
-    }
-
-    [JsonConverter(typeof(JsonPathConverter))]
-    public class ReadDataCiteModel
-    {
-        #region data
-
-        [JsonProperty("data.id")]
-        public string Id { get; set; }
-
-        [JsonProperty("data.type")]
-        public DataCiteType Type { get; set; }
-
-        #endregion
-
-        public static ReadDataCiteModel Deserialize(string json)
-        {
-            var jsonSettings = new JsonSerializerSettings
-            {
-                NullValueHandling = NullValueHandling.Ignore,
-                DefaultValueHandling = DefaultValueHandling.IgnoreAndPopulate
-            };
-
-            return JsonConvert.DeserializeObject<ReadDataCiteModel>(json, jsonSettings);
+            return JsonConvert.DeserializeObject<DataCiteModel>(json, jsonSettings);
         }
     }
 
