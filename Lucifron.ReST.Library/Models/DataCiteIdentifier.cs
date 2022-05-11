6160cf800a49b80cd35a95a8be8c5d3eb29581c7
Sven Thiel <55701246+sventhiel@users.noreply.github.com>
2022-05-11 11:39:28 +0200

    add concept of identifiers section as model

--- Lucifron.ReST.Library/Models/DataCiteIdentifier.cs
+++ Lucifron.ReST.Library/Models/DataCiteIdentifier.cs
@@ -1,12 +1,35 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Lucifron.ReST.Library.Models
 {
-    internal class DataCiteIdentifier
+    public class DataCiteIdentifier
     {
+        [JsonProperty("identifier")]
+        public string Identifier { get; set; }
+
+        [JsonProperty("identifierType")]
+        public DataCiteIdentifierType IdentifierType { get; set; }
+
+        [JsonConstructor]
+        protected DataCiteIdentifier()
+        { }
+
+        public DataCiteIdentifier(string identifier, DataCiteIdentifierType identifierType)
+        {
+            Identifier = identifier;
+            IdentifierType = identifierType;
+        }
+    }
+
+    public enum DataCiteIdentifierType
+    {
+        [EnumMember(Value = "DOI")]
+        DOI = 1
     }
 }
