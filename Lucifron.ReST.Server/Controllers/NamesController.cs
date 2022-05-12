df397393f3d00e34fffc8a86312b29ef719e0217
Sven Thiel <55701246+sventhiel@users.noreply.github.com>
2022-05-12 09:44:39 +0200

    add model and controller logic for "human" name resolution

---
+++ Lucifron.ReST.Server/Controllers/NamesController.cs
@@ -0,0 +1,26 @@
+﻿using Lucifron.ReST.Server.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Lucifron.ReST.Server.Controllers
+{
+    public class NamesController : ApiController
+    {
+        [ApiAuth]
+        public HttpResponseMessage Get(string name)
+        {
+            try
+            {
+                return Request.CreateResponse(HttpStatusCode.OK);
+            }
+            catch
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest);
+            }
+        }
+    }
+}
