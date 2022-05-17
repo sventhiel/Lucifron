e93b6e120e42ea194cd3ff6421a7bf4aa95c7a3f
Sven Thiel <55701246+sventhiel@users.noreply.github.com>
2022-05-17 09:56:20 +0200

    update doi helper for pattern checks

---
+++ Lucifron.ReST.Server/Helpers/DOIHelper.cs
@@ -0,0 +1,58 @@
+﻿using Fare;
+using LiteDB;
+using Lucifron.ReST.Server.Services;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Web;
+
+namespace Lucifron.ReST.Server.Helpers
+{
+    public class DOIHelper
+    {
+        public static string Create(string prefix, string pattern, Dictionary<string, string> placeholders)
+        {
+            try
+            {
+                // check placeholders and replace them
+                if (placeholders != null)
+                {
+                    foreach (var placeholder in placeholders)
+                    {
+                        pattern = pattern.Replace(placeholder.Key, placeholder.Value);
+                    }
+                }
+
+                // create a random suffix that matches the pattern and return it
+                Xeger xeger = new Xeger($"{pattern}", new Random());
+                return $"{prefix}/{xeger.Generate()}";
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+
+        public static bool Validate(string doi, string prefix, string pattern, Dictionary<string, string> placeholders = null)
+        {
+            try
+            {
+                if (placeholders != null)
+                {
+                    foreach (var placeholder in placeholders)
+                    {
+                        pattern = pattern.Replace(placeholder.Key, placeholder.Value);
+                    }
+                }
+                Regex rg = new Regex($"{prefix}/{pattern}");
+                return rg.IsMatch(doi);
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
